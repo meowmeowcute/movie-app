78f98969dd101c77e5c2748260cf91bce68b3340
Liliya Rizvanova <liliya.rizvanova@cxense.com>
2017-07-02 01:10:32 +0400

    step 1: render the list of movies

--- public/movie-app.js
+++ public/movie-app.js
@@ -9,5 +9,49 @@ var Movies = Backbone.Collection.extend({
 });
 
 $(document).ready(function() {
-  // Backbone App setup here
+    // Backbone App setup here
+    //define individual movie view
+    var MovieView = Backbone.View.extend({
+      tagName: "article",
+      classname: "movie",
+      template: _.template($("#movieTemplate").html()),
+
+      render: function() {
+        this.$el.html(this.template(this.model.toJSON()));
+        return this;
+      }
+    });
+
+    var MovieList = Backbone.View.extend({
+        el: $("#movies"),
+
+        initialize: function() {
+          var self = this;
+          this.collection = new Movies([]);
+          this.collection.fetch({
+              success: function(collection,response,options) {
+                  self.render();
+              }
+          });
+        },
+
+        render: function() {
+          this.$el.find("article").remove();
+          _.each(this.collection.models, function(item, index, list) {
+            this.renderMovie(item);
+          }, this);
+        },
+
+        renderMovie: function(item) {
+          var movieView = new MovieView({
+              model: item
+          });
+
+          this.$el.append(movieView.render().el);
+        }
+    });
+
+    var movieList = new MovieList();
+    Backbone.history.start();
+
 });
