9da2935978480a0605350be13eb7cb8649a22571
Liliya Rizvanova <liliya.rizvanova@cxense.com>
2017-07-05 20:43:33 +0400

    step 2: add ability to add new movies

--- public/movie-app.js
+++ public/movie-app.js
@@ -8,13 +8,23 @@ var Movies = Backbone.Collection.extend({
   'model': Movie
 });
 
+var Genre = Backbone.Model.extend({
+    'urlRoot': '/genres',
+    'idAttribute': 'pk'
+});
+
+var Genres = Backbone.Collection.extend({
+    'url': '/genres',
+    'model': Genre
+});
+
 $(document).ready(function() {
     // Backbone App setup here
     //define individual movie view
     var MovieView = Backbone.View.extend({
-      tagName: "article",
-      classname: "movie",
-      template: _.template($("#movieTemplate").html()),
+      tagName: 'article',
+      classname: 'movie',
+      template: _.template($('#movieTemplate').html()),
 
       render: function() {
         this.$el.html(this.template(this.model.toJSON()));
@@ -23,7 +33,11 @@ $(document).ready(function() {
     });
 
     var MovieList = Backbone.View.extend({
-        el: $("#movies"),
+        el: $('#movies'),
+
+        events: {
+            'click #submit-movie': 'createMovie'
+        },
 
         initialize: function() {
           var self = this;
@@ -33,10 +47,17 @@ $(document).ready(function() {
                   self.render();
               }
           });
+
+          this.genres = new Genres();
+          this.genres.fetch({
+            success: function(collection,response,options) {
+                  self.populateGenreSelector();
+            }
+          });
         },
 
         render: function() {
-          this.$el.find("article").remove();
+          this.$el.find('article').remove();
           _.each(this.collection.models, function(item, index, list) {
             this.renderMovie(item);
           }, this);
@@ -48,6 +69,50 @@ $(document).ready(function() {
           });
 
           this.$el.append(movieView.render().el);
+        },
+
+        createMovie: function(e) {
+            e.preventDefault();
+
+            var formData = {};
+            formData.name = this.$el.find('#movie-form #movie-name').val();
+            formData.genre_fks = [];
+
+            var genres = this.$el.find('#movie-form #movie-genre').val();
+            _.each(genres, function(item, index, list) {
+                formData.genre_fks.push(item);
+            });
+
+            if (this.validateMovie(formData)) {
+                var self = this;
+                var movie = new Movie({
+                    'name': formData.name,
+                    'genre_fks' : formData.genre_fks
+                });
+                movie.save(null, {'success': function() {
+                    self.initialize();
+                }});
+                $('#movie-form')[0].reset();
+            } else {
+                alert("Please enter movie name");
+            }
+        },
+
+        populateGenreSelector: function() {
+            var genreSelector = this.$el.find('#movie-genre');
+            _.each(this.genres.models, function(item, index, list) {
+                var option = $('<option/>', {
+                    value: item.attributes.pk,
+                    text: item.attributes.name
+                }).appendTo(genreSelector);
+            });
+        },
+
+        validateMovie: function(formData) {
+            if (formData.name === "") {
+                return false;
+            }
+            return true;
         }
     });
 
